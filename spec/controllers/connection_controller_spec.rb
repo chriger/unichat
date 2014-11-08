3ab73bde04158470829dd120ba4dc653a3c6e1ea
cgerloff <cgerloff@fuchs.de>
2014-11-08 12:23:09 +0100

    Refactor controller spec

--- spec/controllers/connection_controller_spec.rb
+++ spec/controllers/connection_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe 'ConnectionController' do
   context '#pong' do
     it "should send a 'pong' message" do
-      expect(create_event('connection.ping', 'foobar').dispatch).to trigger_message
+      expect(create_event('connection.ping', 'ping').dispatch).to trigger_message 'pong'
     end
   end
 end
