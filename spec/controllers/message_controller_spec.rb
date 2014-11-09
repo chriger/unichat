2766d79a32d90a024b2ee9f257b1c667d7b8c51a
chriger <christiangrlofff@googlemail.com>
2014-11-09 09:53:15 +0100

    Pending spec for messaging queue

--- spec/controllers/message_controller_spec.rb
+++ spec/controllers/message_controller_spec.rb
@@ -1,4 +1,11 @@
 require 'rails_helper'
 
 describe 'MessageController' do
+  context '#send' do
+    let(:send) { create_event('message.send', 'Foobar').dispatch }
+
+    it 'should pass message to message queue' do
+      pending 'TODO direct messages to queue'
+    end
+  end
 end
