05fef7a71f087baa80d4fe2620f804fac13f81d5
chriger <christiangrlofff@googlemail.com>
2014-12-15 16:23:11 +0100

    Add authentification to chat controller

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -1,7 +1,11 @@
 class ChatController < WebsocketRails::BaseController
+  before_filter :authenticate_user!
   def send_message
-
-    trigger_success( { message: 'it works' } )
+    if AmqpPublisher.publish_message(message)
+      trigger_success( { message: 'message send' } )
+    else
+      trigger_failure( { message: 'failed to send message'})
+    end
   end
 
   def get_messages
